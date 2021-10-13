6d3e01035490d291d9649da5e9ca903f80116b19
Pere Urbon <pere@confluent.io>
2021-10-13 13:40:26 +0200

    add test for schema mappings in sqlserver custom dialect

---
+++ CustomJdbcDialog/src/test/java/com/purbon/kafka/connect/jdbc/TestCustomSqlServerJdbcDialect.java
@@ -0,0 +1,30 @@
+package com.purbon.kafka.connect.jdbc;
+
+
+import io.confluent.connect.jdbc.dialect.BaseDialectTest;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Timestamp;
+import org.junit.Test;
+
+public class TestCustomSqlServerJdbcDialect  extends BaseDialectTest<CustomSqlServerJdbcDialect> {
+
+    @Override
+    protected CustomSqlServerJdbcDialect createDialect() {
+        return new CustomSqlServerJdbcDialect(sourceConfigWithUrl("jdbc:sqlserver://something"));
+    }
+
+    @Test
+    public void shouldMapDataTypes() {
+        verifyDataTypeMapping("bigint", Schema.INT64_SCHEMA);
+        verifyDataTypeMapping("varchar(max)", Schema.STRING_SCHEMA);
+
+        var debeziumNanoTimeSchema = SchemaBuilder.int64().name(DebeziumTimeUnits.NANOS_TIMESTAMP).version(1).build();
+        verifyDataTypeMapping("datetime2(7)", debeziumNanoTimeSchema);
+
+        var debeziumMillisTimeSchema = SchemaBuilder.int64().name(DebeziumTimeUnits.MILLIS_TIMESTAMP).version(1).build();
+        verifyDataTypeMapping("datetime", debeziumMillisTimeSchema);
+
+
+    }
+}
